b6ab63d3684f86a3b96b77f4ca993bc5b38855e8
Arkadiusz <arkadiusz.krupinski15@gmail.com>
2020-03-15 14:04:16 +0100

    dodanie metody - obsluga powiadomien oraz wpisanie wartosci do textView

--- app/src/main/java/com/example/my_application/MainActivity.java
+++ app/src/main/java/com/example/my_application/MainActivity.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+//    potrzebne do pisania logow w Logcat
     private static final String TAG = "onCreatedMethodSHOW";
+
+//    wypisywanie daty w elemncie TextView (android)
+    TextView textView_output;
+
 
     //    lifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        pisanie logów w Logcat
         Log.i(TAG, "onCreate CALLED");
+
+//        zrob referencje do textView_output elementu o id: output;
+        textView_output = findViewById(R.id.output);
     }
 
     //    lifecycle
@@ -50,5 +64,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+//    wyskoczy tekst kliknales przycisk AC
+    public void clickHandler(View view) {
+        Date date = new Date();
+
+        textView_output.setText("Dzis jest "+ date.toString());
+
+        Toast.makeText(this,"Kliknales przycisk AC", Toast.LENGTH_LONG).show();
     }
 }
