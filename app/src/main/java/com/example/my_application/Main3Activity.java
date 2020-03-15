e5805e6f04ed8ef583cc6710e5dec88ca493c05c
Arkadiusz <arkadiusz.krupinski15@gmail.com>
2020-03-15 20:48:29 +0100

    Dzialajace przyciski - jest wyswietlenie po kliknieciu -> = <- TODO: logika kalkulatora

--- app/src/main/java/com/example/my_application/Main3Activity.java
+++ app/src/main/java/com/example/my_application/Main3Activity.java
@@ -3,9 +3,11 @@ package com.example.my_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -15,7 +17,9 @@ public class Main3Activity extends AppCompatActivity {
 
     String stringContainer = "";
 
-    public void setButtons() {
+    public void setButtons()
+//    ustawia wszystkie przyciski, uzywajac findViewById()
+    {
         this.btn_ac = findViewById(R.id.ac);
         this.btn_del = findViewById(R.id.del);
         this.btn_percent = findViewById(R.id.percent);
@@ -38,19 +42,36 @@ public class Main3Activity extends AppCompatActivity {
         this.btn_equals = findViewById(R.id.equals);
     }
 
-    public void setTextView() {
+    public void setTextView()
+//    ustawia wszystkie textViews, uzywajac findViewById()
+    {
         textView_input = findViewById(R.id.input);
         textView_output = findViewById(R.id.output);
     }
 
-    public String addToStringContainer(Button button) {
+    public String addToStringContainer(Button button)
+//    dodaje symbole do pojemnika na string
+    {
         this.stringContainer = this.stringContainer + button.getText().toString();
         return this.stringContainer;
     }
 
+    public String removeLastCharacter(String str) {
+        String result = "";
+        if ((str != null) && (str.length() > 0)) {
+            result = str.substring(0, str.length() - 1);
+        }
+        Toast.makeText(this, "value is " + result, Toast.LENGTH_SHORT).show();
+        return result;
+
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+//    gdy stworzysz aplikację
+    {
         super.onCreate(savedInstanceState);
+//        ustaw activity_main3
         setContentView(R.layout.activity_main3);
 
 //        findViewById buttons
@@ -61,8 +82,28 @@ public class Main3Activity extends AppCompatActivity {
         btn_ac.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                czyści stringContainer oraz wszystkie textViewsy
+
+//                to jest w androidzie
                 textView_input.setText("");
                 textView_output.setText("");
+
+//                to jest w javce => class String
+                stringContainer = "";
+            }
+        });
+
+        btn_nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_nine);
+            }
+        });
+
+        btn_eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_eight);
             }
         });
 
@@ -73,9 +114,109 @@ public class Main3Activity extends AppCompatActivity {
             }
         });
 
+        btn_six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_six);
+            }
+        });
+
+        btn_five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_five);
+            }
+        });
+
+        btn_four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_four);
+            }
+        });
+
+        btn_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_three);
+            }
+        });
+
+        btn_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_two);
+            }
+        });
+
+        btn_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_one);
+            }
+        });
+
+        btn_zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_zero);
+            }
+        });
+
+        btn_dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_dot);
+            }
+        });
+
+        btn_percent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_percent);
+            }
+        });
+
+        btn_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_division);
+            }
+        });
+
+        btn_multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_multiply);
+            }
+        });
+
+        btn_subtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_subtraction);
+            }
+        });
+
+        btn_addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = addToStringContainer(btn_addition);
+            }
+        });
+
+        btn_del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringContainer = removeLastCharacter(stringContainer);
+            }
+        });
+
+//        równa się?
         btn_equals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                
                 textView_output.setText(stringContainer);
             }
         });
